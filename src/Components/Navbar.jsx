8af60384406e4f59c817a72196630a115d577444
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-01-26 00:15:31 +0600

    deployed navbar components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router";
 import { AuthContext } from "../Context/AuthContext";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, setUser, signOutUserFunc } = useContext(AuthContext);
   console.log(user);
+
+  const handleSignOut = () => {
+    // signOut(auth)
+    signOutUserFunc()
+      .then(() => {
+        toast.success("Sign Out Succesfull!");
+        setUser(null);
+      })
+      .catch((e) => {
+        toast.error(e.message);
+      });
+  };
+
   return (
     <div>
       <nav className="bg-indigo-600 text-white px-8 py-4">
@@ -25,13 +39,40 @@ const Navbar = () => {
             >
               Register
             </NavLink>
+          </div>
+          {/* <button className="btn primary"> log</button> */}
+          {user ? (
+            <div className="relative group text-center flex justify-between items-center gap-[20px] space-y-3">
+              <img
+                src={user?.photoURL || "https://via.placeholder.com/88"}
+                className="h-[40px] mb-[0px]  rounded-full mx-auto"
+                alt=""
+              />
+
+              <div
+                className="absolute top-full left-1/2 -translate-x-1/2 mt-2 
+                  hidden group-hover:block 
+                  bg-black text-white text-sm px-3 py-1 rounded whitespace-nowrap"
+              >
+                {user?.displayName || "No Name"}
+              </div>
+              {/* <h2 className="text-xl font-semibold">{user?.displayName}</h2> */}
+              {/* <p>{user?.email}</p> */}
+              <button
+                onClick={handleSignOut}
+                className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
+              >
+                Sign Out
+              </button>
+            </div>
+          ) : (
             <NavLink
               to={"/login"}
               className={({ isActive }) => (isActive ? "text-purple-500" : "")}
             >
               Login
             </NavLink>
-          </div>
+          )}
         </div>
       </nav>
     </div>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -37,18 +37,6 @@ const Login = () => {
       });
   };
 
-  const handleSignOut = () => {
-    // signOut(auth)
-    signOutUserFunc()
-      .then(() => {
-        toast.success("Sign Out Succesfull!");
-        setUser(null);
-      })
-      .catch((e) => {
-        toast.error(e.message);
-      });
-  };
-
   const handleGoogleSignIn = () => {
     // signInWithPopup(auth, googleProvider)
     signInWithEmailFunc()
@@ -83,85 +71,67 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-md">
-        {user ? (
-          <div className="text-center space-y-3">
-            <img
-              src={user?.photoURL || "https://via.placeholder.com/88"}
-              className="h-20 w20 rounded-full mx-auto"
-              alt=""
+        <form onSubmit={handleLogIn} className="space-y-4">
+          <h2 className="text-2xl font-semibold text-center mb-6">
+            Login to your account
+          </h2>
+          {/* Email */}
+          <div>
+            <label className="block text-sm font-medium mb-1">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
-            <h2 className="text-xl font-semibold">{user?.displayName}</h2>
-            <p>{user?.email}</p>
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block text-sm font-medium mb-1">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+
+          <button
+            className="cursor-pointer"
+            onClick={handlResetPassword}
+            type="button"
+          >
+            Reset Password!
+          </button>
+
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
+          >
+            Login
+          </button>
+
+          <div className="py-10 flex justify-center items-center ">
             <button
-              onClick={handleSignOut}
-              className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
+              onClick={handleGoogleSignIn}
+              className="p-2 w-full font-semibold bg-gray-300 rounded-full cursor-pointer"
             >
-              Sign Out
+              Continue With Google Login
             </button>
           </div>
-        ) : (
-          <form onSubmit={handleLogIn} className="space-y-4">
-            <h2 className="text-2xl font-semibold text-center mb-6">
-              Login to your account
-            </h2>
-            {/* Email */}
-            <div>
-              <label className="block text-sm font-medium mb-1">Email</label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-            </div>
 
-            {/* Password */}
-            <div>
-              <label className="block text-sm font-medium mb-1">Password</label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-            </div>
-
-            <button
-              className="cursor-pointer"
-              onClick={handlResetPassword}
-              type="button"
-            >
-              Reset Password!
-            </button>
-
-            <button
-              type="submit"
-              className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
-            >
-              Login
-            </button>
-
-            <div className="py-10 flex justify-center items-center ">
-              <button
-                onClick={handleGoogleSignIn}
-                className="p-2 w-full font-semibold bg-gray-300 rounded-full cursor-pointer"
-              >
-                Continue With Google Login
-              </button>
-            </div>
-
-            <p className="text-sm text-center mt-4">
-              Don’t have an account?{" "}
-              <Link to="/register" className="text-indigo-600 hover:underline">
-                Register
-              </Link>
-            </p>
-          </form>
-        )}
+          <p className="text-sm text-center mt-4">
+            Don’t have an account?{" "}
+            <Link to="/register" className="text-indigo-600 hover:underline">
+              Register
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
