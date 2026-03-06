e7cf8f5bc954b37302db5a64df22309d69e5839b
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-03-06 23:09:00 +0600

    deployed Error 404 page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import error_404 from "../assets/error-404.png";
 
 const ErrorPage = () => {
-    return (
+  return (
+    <div>
+      <div className="bg-[#F5F5F5] flex flex-col items-center justify-center p-5 lg:p-20 md:p-10  space-y-3">
         <div>
-            <h1>404 Error Page!</h1>
+          <img src={error_404} alt="" />
         </div>
-    );
+        <div className="text-center space-y-3">
+          <h1 className="text-5xl font-semibold text-[#001931]">
+            Oops, page not found!
+          </h1>
+          <p className="text-xl font-normal ">
+            The page you are looking for is not available.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
